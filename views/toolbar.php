d8a03840a31d1e95d9e5f7e007cbd0d25651bad7
ControlledChaos <greg@ccdzine.com>
2023-10-26 10:22:49 -0700

    Toolbar markup & CSS.

--- views/toolbar.php
+++ views/toolbar.php
@@ -36,24 +36,22 @@ if ( $user->profilePicture() ) {
 
 ?>
 <section id="admin-toolbar" class="admin-toolbar" data-admin-user-toolbar>
-	<nav class="user-action">
+	<nav class="admin-toolbar-nav toolbar-user-action">
 		<ul class="admin-toolbar-nav-list">
+			<?php if ( $site->logo() ) : ?>
 			<li>
-				<a href="<?php echo DOMAIN_ADMIN;?>" target="_blank"><?php $L->p( 'Site Admin' ); ?></a>
+				<a class="admin-toolbar-logo-link" target="_blank" href="<?php echo HTML_PATH_ROOT; ?>" title="<?php $L->p( 'View Site' ); ?>">
+					<img class="admin-toolbar-logo" src="<?php echo $site->logo(); ?>" alt="<?php $L->p( 'View Site' ); ?>" />
+				</a>
+			</li>
+			<?php else : ?>
+			<li>
+				<a class="admin-toolbar-logo-link" target="_blank" href="<?php echo HTML_PATH_ROOT; ?>"><?php $L->p( 'View Site' ); ?></a>
+			</li>
+			<?php endif; ?>
 
-				<ul>
-					<li>
-						<a href="<?php echo HTML_PATH_ADMIN_ROOT . 'dashboard'; ?>"><?php $L->p( 'Dashboard' ); ?></a>
-					</li>
-
-					<li>
-						<a target="_blank" href="<?php echo HTML_PATH_ROOT; ?>"><?php $L->p( 'View Site' ); ?></a>
-					</li>
-
-					<li>
-						<a href="<?php echo HTML_PATH_ADMIN_ROOT . 'edit-user/' . $login->username(); ?>"><?php $L->p( 'Your Profile' ); ?></a>
-					</li>
-				</ul>
+			<li>
+				<a href="<?php echo HTML_PATH_ADMIN_ROOT . 'dashboard'; ?>"><?php $L->p( 'Dashboard' ); ?></a>
 			</li>
 
 			<li>
@@ -129,16 +127,22 @@ if ( $user->profilePicture() ) {
 			<?php endif; ?>
 		</ul>
 	</nav>
-	<nav class="user-info">
+	<nav class="admin-toolbar-nav toolbar-user-info">
 		<ul class="admin-toolbar-nav-list">
 			<li>
 				<a id="profile-link" href="<?php echo $profile; ?>">
-					<img class="avatar user-avatar user toolbar-avatar" src="<?php echo $avatar; ?>" width="24"> <span><?php echo $name; ?></span>
+					<img class="avatar user-avatar user toolbar-avatar admin-toolbar-avatar" src="<?php echo $avatar; ?>" width="24"> <span><?php echo $name; ?></span>
 				</a>
-			</li>
 
-			<li>
-				<a id="toolbar-logout" href="<?php echo HTML_PATH_ADMIN_ROOT . 'logout'; ?>"><?php $L->p( 'Logout' ); ?></a>
+				<ul class="user-actions-sublist">
+					<li>
+						<a href="<?php echo HTML_PATH_ADMIN_ROOT . 'edit-user/' . $login->username(); ?>"><?php $L->p( 'Your Profile' ); ?></a>
+					</li>
+
+					<li>
+						<a id="toolbar-logout" href="<?php echo HTML_PATH_ADMIN_ROOT . 'logout'; ?>"><?php $L->p( 'Log Out' ); ?></a>
+					</li>
+				</ul>
 			</li>
 		</ul>
 	</nav>
